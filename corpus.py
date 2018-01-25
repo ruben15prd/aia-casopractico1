fc8bbdaf5374eda2546802486cdc9d1d9a3c44d9
ruben15prd <ruben15prd@hotmail.es>
2018-01-25 12:03:29 +0100

    Actualizado el metodo Corpus

--- corpus.py
+++ corpus.py
@@ -10,7 +10,9 @@ def generaCorpus():
     
     #Diccionario clave: numero - valores : letras
     
-    #Estructura palabras: clave: nºocurrencias,numeros correspondientes a la clave lista: [la palabra anterior, los numeros de esa palabra,  nº de veces que aparece]  
+    #Estructura palabras: clave: nºocurrencias,numeros correspondientes a la clave - lista: [la palabra anterior, los numeros de esa palabra,  nº de veces que aparece]  
+    #Clave : 385723 Valor: [4,casa,[42,la,9]]
+    
     #El número de caracteres se calcularia a demanda mediante el .length
     # Cuando tengamos un bigram de palabras cuando estemos consultando la segunda palabra, la primera ya no son números
     # Cuando no obtengamos resultado haciendo un bigram, hay que usar el modelo anterior.
@@ -50,10 +52,81 @@ def generaCorpus():
     
     #Eliminamos los caracteres especiales y no utiles y lo formateamos
     texto = texto.lower()
-    textoSinCaracteresRaros = re.sub('[^a-zA-Z0-9-_*. áéíóúÁÉÍÓÚüÜñÑ]', '', texto)
-    textoSinCaracteresRaros = re.sub('[ +]', '-', textoSinCaracteresRaros)
-    textoSinCaracteresRaros = re.sub('-{2,}', '', textoSinCaracteresRaros)
-    textoSinCaracteresRaros = re.sub('-', ' ', textoSinCaracteresRaros)
-    print(textoSinCaracteresRaros)
+    textoSinCaracteresEspeciales = re.sub('[^a-zA-Z0-9-_*. áéíóúÁÉÍÓÚüÜñÑ]', '', texto)
+    textoSinCaracteresEspeciales = re.sub('[ +]', '-', textoSinCaracteresEspeciales)
+    textoSinCaracteresEspeciales = re.sub('-{2,}', '', textoSinCaracteresEspeciales)
+    textoSinCaracteresEspeciales = re.sub('-', ' ', textoSinCaracteresEspeciales)
+    #print(textoSinCaracteresEspeciales)
     
+    
+    #Dividimos el texto por los puntos
+    textoLista = textoSinCaracteresEspeciales.split(".");
+    
+    palabraAnterior = ''
+    
+    for frase in textoLista:
+        palabras = frase.split(" ")
+        palabras = eliminaElementosLongitudMenor1(palabras)
+        #print(palabras)
+        count = 0
+        while (count < len(palabras)):
+            palabra = palabras[count]
+            
+            if count == 0:
+                palabra = palabras[0]
+                #numerosClave = codificar(palabra,diccionarioLetrasNumeros)
+                #insertaPalabra(corpusPalabras, numerosClave)
+                
+            else:
+                palabra = palabras[count]
+            
+            
+            count = count + 1
+    
+    hola = ValorPalabra(3,"cosa","34", "si",3)
+    corpusPalabras = insertaPalabra(corpusPalabras,'5742',hola)
+    print(corpusPalabras)
+                
+   
+    
+
+
+
+
+            
+def eliminaElementosLongitudMenor1(lista):
+    listaMayor1 = []
+    for elem in lista:
+        if len(elem) >= 1:
+            listaMayor1.append(elem)
         
+    return listaMayor1   
+    
+def insertaPalabra(corpusPalabras, clave, palabraAnterior):
+    corpusPalabras[clave] = palabraAnterior
+    return corpusPalabras
+ 
+    
+def codificar(palabra,diccionarioCodificacion):
+    cadenaCodificada = ''
+    for letra in palabra:
+        for elem in diccionarioCodificacion:
+            if letra in diccionarioCodificacion[elem]:
+               cadenaCodificada = cadenaCodificada + elem 
+               
+    return cadenaCodificada
+    
+
+class ValorPalabra:
+    def __init__(self, numOcurrenciasClave, claveSinCodificar, palabraAnteriorCodificada, palabraAnteriorSinCodificar, numOcurrenciasPalabraAnterior):
+        self.numOcurrenciasClave = numOcurrenciasClave
+        self.claveSinCodificar = claveSinCodificar
+        self.palabraAnteriorCodificada = palabraAnteriorCodificada
+        self.palabraAnteriorSinCodificar = palabraAnteriorSinCodificar
+        self.numOcurrenciasPalabraAnterior = numOcurrenciasPalabraAnterior
+        
+    def __str__(self):
+        return str(self.numOcurrenciasClave) + "-" + str(self.claveSinCodificar) + "-" + str(self.palabraAnteriorCodificada) + "-" + str(self.palabraAnteriorSinCodificar) + "-" + str(self.numOcurrenciasPalabraAnterior)
+
+    def __repr__(self):
+        return str(self.numOcurrenciasClave) + "-" + str(self.claveSinCodificar) + "-" + str(self.palabraAnteriorCodificada) + "-" + str(self.palabraAnteriorSinCodificar) + "-" + str(self.numOcurrenciasPalabraAnterior)
